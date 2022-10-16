a7357bed0f95eb6ebd557b0ae162d7384a9258c5
Ben Johnson <benjohnsonv98@gmail.com>
2022-10-16 19:46:17 +0530

    Initial commit

---
+++ Fundamentals/AddNumbers.cpp
@@ -0,0 +1,17 @@
+/**
+ * Function to Add Two Integer Numbers 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Enter the Value For A:"<<endl;
+    cin>>a;
+    cout<<"Enter the Value For B:"<<endl;
+    cin>>b;
+    c = a + b ;
+    cout<<"Result :"<<endl<<c;
+    
+}
